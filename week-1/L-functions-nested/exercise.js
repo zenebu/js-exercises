4f0bd661d087dd19f9775eadccd6aab5647c4141
Zenebu <zenebuge@gmail.com>
2019-03-09 22:52:22 +0000

    completed exercise L

---
+++ week-1/L-functions-nested/exercise.js
@@ -0,0 +1,9 @@
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var sum = 15 + 8;
+var percentageOfStudents = (15 / 23) * 100;
+var percentageOfMentors = (8 / 23) * 100;
+var roughPercentageOfStudents = Math.round(percentageOfStudents);
+var roughPercentageOfMentors = Math.round(percentageOfMentors);
+console.log("Percentage students:" + roughPercentageOfStudents + "%");
+console.log("Percentage mentors:" + roughPercentageOfMentors + "%");
--- week-1/L-functions-nested/exercise2.js
+++ week-1/L-functions-nested/exercise2.js
@@ -1,5 +1,17 @@
+function greeting() {
+  return "HELLO";
+}
+function upperCase(name) {
+  return name.toUpperCase();
+}
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+console.log(upperCaseNames(mentor1));
+console.log(upperCaseNames(mentor2));
+console.log(upperCaseNames(mentor3));
+console.log(upperCaseNames(mentor4));
+console.log(upperCaseNames(mentor5));
