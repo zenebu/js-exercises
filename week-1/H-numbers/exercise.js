cce3cd76d590f633dc4a2f6c37e301a128df5540
Zenebu <zenebuge@gmail.com>
2019-03-09 22:36:10 +0000

    creating variable with a number

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,9 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+console.log("Number of students: 15");
+console.log("Number of mentors: 8");
+console.log(
+  "Total number of students and mentors: " +
+    (numberOfStudents + numberOfMentors)
+);
