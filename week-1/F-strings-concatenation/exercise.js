c9e1e09ca1f39fb7d71538e719064256f31c16ea
Zenebu <zenebuge@gmail.com>
2019-03-09 22:27:54 +0000

    completed exercise F

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `name`
+var greetingStart = "Hello, my name is ";
+var name = "Daniel";
 
-console.log(name);
+var greeting = greetingStart + name;
+console.log(greeting);
