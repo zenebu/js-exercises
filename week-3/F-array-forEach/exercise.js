f318ec0751e48b7f677fd9047f7a3466ac5e7945
Zenebu <zenebuge@gmail.com>
2019-03-23 16:19:41 +0000

    completed week-3 exercise F

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -9,7 +9,19 @@
 */
 
 // Complete this function -->
-function fizzBuzz() {}
+function shiftNumbersInToFuzzBuzz(num) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else console.log(num);
+}
+
+function fizzBuzz(array) {
+  return array.forEach(shiftNumbersInToFuzzBuzz);
+}
 
 // DEBUG
 var sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
