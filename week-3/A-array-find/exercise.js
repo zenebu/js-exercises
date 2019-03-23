cc1f702aaef1590f54d737045cbf3842b9c1278d
Zenebu <zenebuge@gmail.com>
2019-03-23 15:59:14 +0000

    completed week -3 exercise A

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -5,7 +5,9 @@
 */
 
 // -- Complete this function -->
-function findLongNameThatStartsWithA(names) {}
+function findLongNameThatStartsWithA(name) {
+  return name[0] === "A" && name.length > 7;
+}
 
 // DEBUG (check your function works by calling it with some sample input)
 var names = [
@@ -20,7 +22,7 @@ var names = [
   "Ahmed"
 ];
 
-var longNameThatStartsWithA = findLongNameThatStartsWithA(names);
+var longNameThatStartsWithA = names.find(findLongNameThatStartsWithA);
 
 console.log(longNameThatStartsWithA); // Expected output: "Alexandra"
 
