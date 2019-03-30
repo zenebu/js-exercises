27d382d44e899c6157e5e148d7516a67d5e01151
Zenebu <zenebuge@gmail.com>
2019-03-30 13:25:24 +0000

    completed week 4 exercise

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  brand: "Lenovo",
+  screenSize: 13,
+  ram: 32,
+  isTouchscreen: true,
+  operatingSystem: "Linux"
+};
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,32 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var mentor = {
+  name: "Nate",
+  age: 20,
+  job: "coder",
+  hight: 1.65,
+  favouritefood: "burger"
+};
+
+var shoe = {
+  brand: "vans",
+  shoeNumber: 3.5
+};
+
+var food = {
+  name: "pizza",
+  test: "sweet"
+};
+
+var clothe = {
+  brand: "chanel",
+  size: 6,
+  colour: purple
+};
+
+var tvShow = {
+  title: "x-factour",
+  day: "satuday",
+  time: 7.3
+};
